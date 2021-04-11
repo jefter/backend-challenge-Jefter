265b259a15eda73838fde9439b988006ce1325d0
Jefter de Oliveira Gonçalves <jefter.dev@gmail.com>
2021-04-11 13:22:08 -0300

    ok

--- desafio.domain/StatusPedido.cs
+++ desafio.domain/StatusPedido.cs
@@ -8,10 +8,119 @@ namespace desafio.domain
 {
     public class StatusPedido
     {
+        const string APROVADO = "APROVADO";
+        const string REPROVADO = "REPROVADO";
+
         public string status { get; set; }
         public int itensAprovado { get; set; }
         public int valorAprovado { get; set; }
         public string pedido { get; set; }
+
+        public RetornoStatusPedido validate(Pedido pedido)
+        {
+            try
+            {
+
+           
+            int qtdTotalItens;
+            decimal valorTotalPedido;
+            RetornoStatusPedido result = new RetornoStatusPedido();
+            result.status = new List<STATUS>();
+            result.pedido = this.pedido;
+
+            if (pedido == null)
+            {
+                result.status.Add(domain.enumerator.STATUS.CODIGO_PEDIDO_INVALIDO);
+                return result;
+            }
+
+            qtdTotalItens = pedido.GetTotalItens;
+            valorTotalPedido = pedido.GetValorTotal;
+
+
+            //pedido for localizado no banco de dados. status for igual a REPROVADO
+            if (this.status.Equals(REPROVADO))
+            {
+                result.status.Add(domain.enumerator.STATUS.REPROVADO);
+
+            }
+
+            /*
+              pedido for localizado no banco de dados.
+
+itensAprovados for igual a quantidade de itens do pedido.
+
+valorAprovado for igual o valor total do pedido.
+
+status for igual a APROVADO.
+             */
+
+
+            if (qtdTotalItens == this.itensAprovado && valorTotalPedido == this.valorAprovado && this.status.Equals(APROVADO))
+            {
+                result.status.Add(domain.enumerator.STATUS.APROVADO);
+            }
+
+            /*
+             pedido for localizado no banco de dados.
+
+valorAprovado for menor que o valor total do pedido
+
+status for igual a APROVADO
+             */
+
+            if (this.valorAprovado < valorTotalPedido && this.status.Equals(APROVADO))
+            {
+                result.status.Add(domain.enumerator.STATUS.APROVADO_VALOR_A_MENOR);
+            }
+
+            /*
+             * pedido for localizado no banco de dados.
+
+itensAprovados for menor que a quantidade de itens do pedido.
+
+status for igual a APROVADO
+
+             */
+
+            if (this.itensAprovado < qtdTotalItens && this.status.Equals(APROVADO))
+            {
+                result.status.Add(domain.enumerator.STATUS.APROVADO_QTD_A_MENOR);
+            }
+
+
+            /*
+             * pedido for localizado no banco de dados.
+
+valorAprovado for maior que o valor total do pedido
+
+status for igual a APROVADO
+             */
+            if (this.valorAprovado > valorTotalPedido && this.status.Equals(APROVADO))
+            {
+                result.status.Add(domain.enumerator.STATUS.APROVADO_VALOR_A_MAIOR);
+            }
+
+            /*
+             * pedido for localizado no banco de dados.
+
+itensAprovados for maior que a quantidade de itens do pedido.
+
+status for igual a APROVAD
+             */
+            if (this.itensAprovado > qtdTotalItens && this.status.Equals(APROVADO))
+            {
+                result.status.Add(domain.enumerator.STATUS.APROVADO_QTD_A_MAIOR);
+            }
+
+            return result;
+            }
+            catch (Exception e) 
+            {
+
+                throw e;
+            }
+        }
 
     }
 
@@ -30,8 +139,6 @@ namespace desafio.domain
 
 
             return Enumerable.SequenceEqual(this.status, item.status);
-
-
         }
 
     }
